5a90bc2ce864e9c42b25011161c12b5e85b0e818
ihiggins <42596476+ihiggins@users.noreply.github.com>
2020-09-03 21:13:09 -0400

    changed route to api

---
+++ pages/api/parse.tsx
@@ -0,0 +1,64 @@
+
+import { promises as fs } from 'fs';
+
+const parse = require('himalaya');
+
+const path = require('path');
+const publicPath = path.join(__dirname , '../public');
+
+export default async (req, res) => {
+    var url :string = JSON.parse(req.body);
+    
+    var temp = await getData(url)
+    res.statusCode = 200
+    res.json(temp)
+  }
+  
+
+
+
+  
+// handler
+async function getData(url){
+
+  var res = await fetch(url);
+  var html = await res.text();
+  
+  await fs.writeFile("./util/cache.html", html);
+
+  var tag = await getTags();
+  var tree = await makeTree();
+  return {tag:tag , tree:tree};
+
+  };
+
+  var getTags = async function(){
+    var response = await fs.readFile('./util/cache.html', 'utf8');
+        var items = new Object();
+      var rez = response.split("<");
+      for (let i of rez) {
+        if (i.charAt(0).match(/[a-z]/i)) {
+          var div = "";
+          for (let j of i) {
+            if (j.match(/[a-z]/i)) {
+              div += j;
+            } else {
+              break;
+            }
+          }
+          if (items.hasOwnProperty(div)) {
+            items[div] = items[div] + 1;
+          } else {
+            items[div] = 1;
+          }
+        }
+      }
+        return items
+  };
+  
+  var makeTree = async function(){
+    var response =  await fs.readFile('./util/cache.html', 'utf8')
+      const json = parse.parse(response);
+     return json;
+  };
+  
---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import React, { useState } from 'react';
+
+import styles from '../styles/Home.module.css'
+
+import Input from '../public/components/input/input'
+
+export default function Home() {
+  const [inputVal, setInputVal] = useState('');
+  const  handleSubmit = async () =>{
+    let response = await fetch(`/api/parse` , {
+      method: 'post',
+      body: JSON.stringify(inputVal)
+    });
+    let data = await response.json()
+    console.log(data);
+  }
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <p className={styles.description}>
+          Enter a url to see the 
+          <code className={styles.code}>Dom Tree</code>
+        </p>
+        <Input value={inputVal}  onSubmit={handleSubmit} onChangeValue={(e) => setInputVal(e.target.value)}> </Input>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
